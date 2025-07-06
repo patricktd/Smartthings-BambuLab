41d23d43b9aa8186a1a41cebcf85afa75115dcde
patricktd <50158951+patricktd@users.noreply.github.com>
2025-07-06 14:41:23 -0300

    Update discovery.lua

--- src/discovery.lua
+++ src/discovery.lua
@@ -2,13 +2,13 @@ local log = require "log"
 local discovery = {}
 
 function discovery.handle_discovery(driver, _should_continue)
-	log.info("Starting Moonraker Discovery")
+	log.info("Starting BambuLab Discovery")
 
 	local metadata = {
 		type = "LAN",
-		device_network_id = "moonraker klipper device",
-		label = "Klipper 3D Printer",
-		profile = "moonraker-klipper.v1",
+		device_network_id = "bambulab device",
+		label = "BambuLab 3D Printer",
+		profile = "bambulab.v1",
 		manufacturer = "SmartThingsCommunity",
 		model = "v1",
 		vendor_provided_label = nil
