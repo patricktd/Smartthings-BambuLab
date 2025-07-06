400d5986d05087e31139e6efb95246764e3d4734
patricktd <50158951+patricktd@users.noreply.github.com>
2025-07-06 14:13:50 -0300

    Update init.lua

--- bambulab-driver/src/init.lua
+++ bambulab-driver/src/init.lua
@@ -1,33 +1,6 @@
--- require st provided libraries
 local capabilities = require "st.capabilities"
-local Driver = require "st.driver"
-local log = require "log"
 
--- other libraries
-local cosock = require "cosock"
-local http = cosock.asyncify "socket.http"
-local https = cosock.asyncify "ssl.https"
-local ltn12 = require "ltn12"
-local json = require "dkjson"
-
--- require custom handlers from driver package
-local discovery = require "discovery"
-
--- capabilities
-local cap_status = capabilities["pattetech.status"]
-
------------------------------------------------------------------
--- main functions
------------------------------------------------------------------
-
--- log to the console if the verbose log setting is switced on
-function console_log(device, message, log_level)
-
-	if(device.preferences.verboseLog == true) then
-
-		if(log_level == nil) then log_level = 'debug' end
-		log.log({}, log_level, message)
-
-	end
-	
-end
+-- Quando receber mensagem do Node-RED:
+device:emit_event(
+    capabilities["pattetech.status"].jobStatus("printing")
+)
